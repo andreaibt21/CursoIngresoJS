2fa2505b5cc722804dc766264191afe071bac2c8
andreaibt21 <andreaixabella@gmail.com>
2021-02-04 19:03:27 -0300

    ejercicios del 2 al 5

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	let mensajeDeRespuesta;
+
+	nombre = prompt("Ingrese nombre");
+
+	mensajeDeRespuesta = "Su nombre es "+ nombre;
+
+	document.getElementById("txtIdNombre").value = mensajeDeRespuesta;
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,57 @@
+/* Andrea Briceño */
 /*
+
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+ej.: "Usted se llama José y tiene 66 años" 	
+
+EJERCICIO 5
+*/
+// function mostrar()
+// {	
+// 	let nombre;
+// 	let edad; 
+// 	let mensaje;
+// 	nombre = document.getElementById("txtIdNombre").value;
+// 	edad = document.getElementById("txtIdEdad").value;
+
+// 	mensaje = "Usted se llama " + nombre + " y tiene " + edad + " años"; 
+
+
+// 	alert(mensaje);
+// }
+
+/*
+
+Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	
+
+*/
+
+/*
+Andrea Briceño 
+
+Ejercicio 5 bis : 
+
+ se debe mostrar un mensaje :
+
+ "Perez , usted se llama jose y tiene 66 años"
+se debe optener el apellido , de la manera que puedan
+*/
 function mostrar()
 {	
-	alert("ok");
-}
+	let nombre;
+	let edad; 
+	let mensaje;
+	let apellido;
 
+	nombre = document.getElementById("txtIdNombre").value;
+	edad = document.getElementById("txtIdEdad").value;
+	apellido = prompt("Ingrese su apellido: ")
+
+	mensaje = apellido + ", usted se llama " + nombre + " y tiene " + edad + " años."; 
+
+
+	alert(mensaje);
+
+
+}
